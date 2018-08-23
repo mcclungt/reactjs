c33fb4130ba3964c3e819c6c23560f5dc41c1ab5
Tim <timothyjmcclung@gmail.com>
2018-08-23 14:40:46 -0400

    added search function

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -2,20 +2,21 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-	const cardComponent = robots.map((user, index) => {
-		return ( 
-			<Card 
-				key={index} 
-				id={robots[index].id} 
-				name={robots[index].name} 
-				email={robots[index].email}
-				/>
-		)
-	});
-	return(
+	return ( 
 		<div>
-			{cardComponent}
-		</div>	
+			{
+				robots.map((user, index) => {
+					return (
+						<Card 
+							key={index} 
+							id={robots[index].id} 
+							name={robots[index].name} 
+							email={robots[index].email}
+							/>
+					);
+				})
+			}
+		</div>
 	);
 }
 
